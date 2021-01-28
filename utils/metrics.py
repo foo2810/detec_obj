3737f452a98cbda548551678073bbeb73b809851
foo2810 <ntshp.ak@gmail.com>
2021-01-28 20:29:44 +0900

    bug fixed overall

--- utils/metrics.py
+++ utils/metrics.py
@@ -40,13 +40,15 @@ def _get_intersect_area(box_a: torch.Tensor, box_b: torch.Tensor):
     -------
     area:
     """
+    assert _boxes_intersect(box_a, box_b)
 
     x_a = max(box_a[0], box_b[0])
     y_a = max(box_a[1], box_b[1])
     x_b = min(box_a[2], box_b[2])
     y_b = min(box_a[3], box_b[3])
     # intersection area
-    return (x_b - x_a + 1) * (y_b - y_a + 1)
+    # return (x_b - x_a + 1) * (y_b - y_a + 1)
+    return (x_b - x_a) * (y_b - y_a)
 
 def _get_union_area(box_a: torch.Tensor, box_b: torch.Tensor):
     """
@@ -60,6 +62,7 @@ def _get_union_area(box_a: torch.Tensor, box_b: torch.Tensor):
     -------
     area:
     """
+    assert _boxes_intersect(box_a, box_b)
 
     s_a = (box_a[2] - box_a[0]) * (box_a[3] - box_a[1])
     s_b = (box_b[2] - box_b[0]) * (box_b[3] - box_b[1])
@@ -128,37 +131,65 @@ def _ap_per_class_base(boxes_gt: torch.Tensor, labels_gt: torch.Tensor, boxes_pr
     """
 
     if len(labels_pred) == 0:
-        return torch.zeros(1, dtype=torch.float), torch.zeros(1, dtype=torch.float), torch.zeros(1, dtype=torch.bool)
+        # print(' >> Hit: first checkpoint')
+        return torch.zeros(1, dtype=torch.float), torch.zeros(1, dtype=torch.float), torch.ones(1, dtype=torch.bool)
 
     assert torch.all(labels_pred == labels_pred[0])
 
     p = torch.argsort(scores_pred)
-    # batched_iou = torch.vmap(iou)
-    # ious = batched_iou(boxes_gt, boxes_pred)
-    ious = []
-    for bb_pred in boxes_pred:
-        max_iou = 1e-12
-        for bb_gt in boxes_gt:
-            out_t = iou(bb_gt, bb_pred)
-            if max_iou < out_t:
-                max_iou = out_t
-        ious += [max_iou]
-    ious = torch.tensor(ious, dtype=torch.float)
+    mask = labels_gt == labels_pred[0]
+    is_correct = torch.zeros(len(labels_pred), dtype=torch.bool)
+    for bb_gt in boxes_gt[mask]:
+        pre_indice = None
+        for i, bb_pred in enumerate(boxes_pred):
+            iou_t = iou(bb_gt, bb_pred)
+            assert iou_t >= 0 and iou_t <= 1, 'IoU must be in [0, 1].'
+            if pre_indice is None and iou_t >= iou_threshold:
+                is_correct[i] = True
+                pre_indice = i
+            elif pre_indice is not None and iou_t >= iou_threshold:
+                is_correct[pre_indice] = False
+                break
+            # else:
+            #     # These cases do not happen.
 
-    is_correct = ious > iou_threshold
+    # ious = []
+    # for bb_pred in boxes_pred:
+    #     max_iou = 0
+    #     for bb_gt in boxes_gt[mask]:
+    #         out_t = iou(bb_gt, bb_pred)
+    #         if max_iou < out_t:
+    #             max_iou = out_t
+    #     ious += [max_iou]
+    # ious = torch.tensor(ious, dtype=torch.float)
+    # is_correct = ious > iou_threshold
+
     corrects = (is_correct).long()
 
-    masks = [torch.tensor([j for j in range(i+1)], dtype=torch.long) for i in range(labels_pred)]
-    n_gts = torch.long(labels_gt == labels_pred[0]).sum().item()
+    masks = [torch.tensor([j for j in range(i+1)], dtype=torch.long) for i in range(len(labels_pred))]
+    n_gts = (labels_gt == labels_pred[0]).long().sum().item()
+    # print(' > n_groud_truths: {}'.format(n_gts))
 
     precisions = []
     recalls = []
-    for mask in masks:
+    for i, mask in enumerate(masks):
+        assert len(mask) == i+1
         precisions += [torch.sum(corrects[p][mask]).item() / len(mask)]
-        recalls += [torch.sum(corrects[p][mask]).item() / n_gts]
+
+        # ここの処理はどうすればいいのか？
+        if n_gts == 0:
+            # print((' >> Hit: second checkpoint'))
+            recalls += [0]
+        else:
+            t = torch.sum(corrects[p][mask]).item()
+            # print(' > n_corrects: {}'.format(t))
+            recalls += [t / n_gts]
 
     precisions = torch.tensor(precisions)
     recalls = torch.tensor(recalls)
+    assert len(precisions) == len(labels_pred)
+    assert len(recalls) == len(labels_pred)
+    assert recalls[-1] <= 1
 
     return precisions, recalls, is_correct[p]
 
@@ -201,7 +232,12 @@ def ap_per_class(boxes_gt: torch.Tensor, labels_gt: torch.Tensor, boxes_pred: to
     """
 
     precisions, _, is_correct = _ap_per_class_base(boxes_gt, labels_gt, boxes_pred, labels_pred, scores_pred, iou_threshold)
-    ap = precisions[is_correct].mean().item()
+    # print('> precisinos: {}'.format(precisions))
+    # print('> recalls: {}'.format(_))
+    # print('> is_correct: {}'.format(is_correct))
+    # print('='*50)
+    assert len(precisions) != 0
+    ap = (precisions[is_correct].sum() / len(precisions)).item()
     return ap
 
 def ap_interpolated_per_class(boxes_gt: torch.Tensor, labels_gt: torch.Tensor, boxes_pred: torch.Tensor, labels_pred: torch.Tensor, scores_pred: torch.Tensor, iou_threshold: float=0.5) -> float:
@@ -245,10 +281,13 @@ def ap_interpolated_per_class(boxes_gt: torch.Tensor, labels_gt: torch.Tensor, b
 
     # Interpolate
     precisions_interpolated = precisions.clone()
-    max_prec = precisions[-1]
-    for i in range(len(recalls)-1, -1, -1):
-        precisions_interpolated[i] = torch.maximum(precisions[i], max_prec)
-        max_prec = precisions_interpolated[i]
+    if torch.all(recalls == recalls[0]):
+        precisions_interpolated = torch.zeros(len(precisions), dtype=torch.float)
+    else:
+        max_prec = precisions[-1]
+        for i in range(len(recalls)-1, -1, -1):
+            precisions_interpolated[i] = torch.maximum(precisions[i], max_prec)
+            max_prec = precisions_interpolated[i]
     
     eleven_points_prec = []
     for i, r in enumerate(torch.arange(0, 1.1, 0.1)):
@@ -256,6 +295,8 @@ def ap_interpolated_per_class(boxes_gt: torch.Tensor, labels_gt: torch.Tensor, b
             if r <= rec:
                 eleven_points_prec += [precisions_interpolated[j]]
                 break
+            if j == len(recalls)-1:
+                eleven_points_prec += [0]
     eleven_points_prec = torch.tensor(eleven_points_prec)
     ap_interpolated = eleven_points_prec.mean().item()
 
@@ -304,6 +345,8 @@ def AP(boxes_gt: torch.Tensor, labels_gt: torch.Tensor, boxes_pred: torch.Tensor
         else:
             ap_c = ap_per_class(boxes_gt, labels_gt, boxes_pred[mask], labels_pred[mask], scores_pred[mask], iou_threshold)
         aps += [ap_c]
+    
+    assert len(aps) == num_classes
 
     aps = torch.tensor(aps, dtype=torch.float)
 
